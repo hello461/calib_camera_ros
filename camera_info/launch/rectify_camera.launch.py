0bad48132485c21c70b10a90e2ea5cf24d67b3a2
quang <quang2001ht@gmail.com>
2026-01-07 09:36:13 +0700

    Finish

--- camera_info/launch/rectify_camera.launch.py
+++ camera_info/launch/rectify_camera.launch.py
@@ -3,23 +3,46 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     return LaunchDescription([
+        # Publish camera information
         Node(
             package='camera_info',
             executable='camera_info_publisher',
             parameters=[{
-                'camera_info_url': 'package://camera_info/config/test.yaml'
+                'camera_info_url': 'package://camera_info/config/camera_config.yaml'
             }]
         ),
 
+        # Publish front image
         Node(
             package='go2_media',
             executable='publish_front_image'
         ),
 
+        # Publish image rectified
         Node(
             package='image_proc',
             executable="rectify_node",
             name='rectify_node',
             arguments=['image:=/camera/image_raw']
+        ),
+
+        # View image raw from front camera
+        Node(
+            package="image_view",
+            executable="image_view",
+            name='Image_raw',
+            remappings=[
+                ('image', 'camera/image_raw')
+            ]
+        ),
+        
+        # View image rectify from camera
+        Node(
+            package='image_view',
+            executable='image_view',
+            name='Image_rectify',
+            remappings=[
+                ('image', 'image_rect')
+            ]
         )
     ])
